f9962b74995da5693e01ecf276b62a7ec55fc882
webersilv4 <webersilv4@hotmail.com>
2023-05-14 15:45:39 -0300

    Update Puppeteer

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        PAGE_URL: 'https://scraping-images-97srqpxcy-webrcost4-gmailcom.vercel.app',
+        PAGE_URL: 'http://localhost:3000',
     },
 };
 
