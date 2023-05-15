344c8d0b13f81bce6e9aea84d0670392c5f83db7
webersilv4 <webersilv4@hotmail.com>
2023-05-14 21:31:15 -0300

    .

--- src/pages/api/scraping.ts
+++ src/pages/api/scraping.ts
@@ -4,33 +4,29 @@ import puppeteer from 'puppeteer';
 
 export default async function scraping(req: NextApiRequest, res: NextApiResponse) {
     
-    try {
-        if (req.method === 'POST') {
-            const { url } = req.body; 
+    if (req.method === 'POST') {
+        const { url } = req.body; 
 
-            const browser = await puppeteer.launch();
-            const page = await browser.newPage();
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
         
-            await page.goto(url);
+        await page.goto(url);
         
-            const imgList = await page.evaluate(()=>{
+        const imgList = await page.evaluate(()=>{
             
-                const img = <any>document.querySelectorAll('img');
-                const arrayImages: Array<any> = [...img];
+            const img = <any>document.querySelectorAll('img');
+            const arrayImages: Array<any> = [...img];
     
     
-                const imgCapture = arrayImages.map(({src})=>({
-                    src
-                }));
+            const imgCapture = arrayImages.map(({src})=>({
+                src
+            }));
             
-                return imgCapture;
-            });
+            return imgCapture;
+        });
     
-            res.status(200).json(imgList);
-            await browser.close();
-        }
-    } catch (error) {
-        res.json(error);
+        res.status(200).json(imgList);
+        await browser.close();
     }
     
     
